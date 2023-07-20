b5acb777a265958bebdb1d2bb833e619ee98aa64
Koen De Groot <koen.de.groot@organimmo.be>
2023-07-20 15:23:25 +0200

    Handle file selection via file picker in Upload component
    The change adds file input handling to the `Upload.vue` component. Previously, files could only be uploaded via drag and drop. Now, users can also select files via the file picker. For this purpose, a conditional is added to `upload(event)` to handle both `input` and `dataTransfer` events. Plus, resetting `isDragOver` flag is moved before the file processing loop to ensure file rejection doesn't impact the state.

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 ﻿import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCPpWwJAmIRJ38pWsZfyj6jfGZd9r7b9os',
@@ -13,7 +14,9 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 
 const usersCollection = db.collection('users')
+const songsCollection = db.collection('songs')
 
-export { auth, db, usersCollection }
+export { auth, db, usersCollection, storage, songsCollection }
