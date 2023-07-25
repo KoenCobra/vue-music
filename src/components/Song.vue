d862b6b1e268e0b9e636f710a2bffe6ab664b0fb
Koen De Groot <koen.de.groot@organimmo.be>
2023-07-25 15:28:00 +0200

    Update comment count dynamically in Song component
    This commit modifies the Song.vue component to increment the song's comment count in the database every time a new comment is added. It also updates the comment count displayed in the UI to reflect the actual number of comments for the song, ensuring accurate real-time data for users.

--- src/components/Song.vue
+++ src/components/Song.vue
@@ -72,6 +72,11 @@ export default {
 
       await commentsCollection.add(comment)
 
+      this.song.comment_count += 1
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count
+      })
+
       await this.getComments()
 
       this.in_submission = false
@@ -120,7 +125,7 @@ export default {
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ comments.length }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
